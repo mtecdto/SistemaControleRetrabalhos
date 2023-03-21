8096a981e7ecbc6f10e61482b81fbea39592b29d
Matheus Fillipe toyc497 <matheusfillipe497@gmail.com>
2023-03-21 14:03:23 -0300

    Projeto Iniciado

---
+++ retrabalhos/src/main/java/com/api/retrabalhos/RetrabalhosApplication.java
@@ -0,0 +1,13 @@
+package com.api.retrabalhos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetrabalhosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetrabalhosApplication.class, args);
+	}
+
+}
---
+++ retrabalhos/src/test/java/com/api/retrabalhos/RetrabalhosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.retrabalhos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetrabalhosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
